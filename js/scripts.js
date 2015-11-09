d3a570e82ba2bb6441705103f58c0b824bd08565
kindredp <kindredp@gmail.com>
2015-11-09 18:10:29 +0800

    Added mailchimp form, styling improvements

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,22 @@
-$(document).ready(function() {
-  $(".feature-1").hover(function(){
-    $(".feature-preview .feature-1").addClass("show fade-in");
-    $(".feature-preview .feature-1").removeClass("hidden");
-  },
+$(function() {
+  $(".feature-1").hover(function() {
+     $(".feature-preview img").attr("src", "../img/features-send-and-receive.jpg");
+  });
+  // , function() {
+  //    $(".feature-preview img").attr("src", "../img/features-android.jpg");
+  // });
 
-    function(){
-      $(".feature-preview .feature-1").removeClass("show");
-      $(".feature-preview .feature-1").addClass("hidden");
-    }
-  );
 
-  $(".feature-2").hover(function(){
-    $(".feature-preview .feature-2").addClass("show fade-in");
-    $(".feature-preview .feature-2").removeClass("hidden");
-  },
+  $(".feature-2").hover(function() {
+     $(".feature-preview img").attr("src", "../img/features-request.jpg");
+  });
 
-    function(){
-      $(".feature-preview .feature-2").removeClass("show");
-      $(".feature-preview .feature-2").addClass("hidden");
-    }
-  );
+  $(".feature-3").hover(function() {
+     $(".feature-preview img").attr("src", "../img/features-friends.jpg");
+  });
 
-  $(".feature-3").hover(function(){
-    $(".feature-preview .feature-3").addClass("show fade-in");
-    $(".feature-preview .feature-3").removeClass("hidden");
-  },
-
-    function(){
-      $(".feature-preview .feature-3").removeClass("show");
-      $(".feature-preview .feature-3").addClass("hidden");
-    }
-  );
+  $(".feature-4").hover(function() {
+     $(".feature-preview img").attr("src", "../img/features-splash.jpg");
+  });
 });
-
-
 
